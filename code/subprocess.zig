e6541e578570f1afd2d7f51a71e3f45b8de9c52d
iddev5 <ayushtechportal@gmail.com>
2021-09-20 23:19:12 +0530

    Fix formatting of subprocess.zig

--- code/subprocess.zig
+++ code/subprocess.zig
@@ -4,14 +4,14 @@ const page_allocator = std.heap.page_allocator;
 const tagName = std.meta.tagName;
 
 pub fn main() anyerror!void {
-  const args = [_][]const u8{ "ls", "-al" }; // equals to : ls -al
+    const args = [_][]const u8{ "ls", "-al" }; // equals to : ls -al
 
-  // Initialize process
-  var process = try ChildProcess.init(&args, page_allocator);
-  defer process.deinit();
+    // Initialize process
+    var process = try ChildProcess.init(&args, page_allocator);
+    defer process.deinit();
 
-  std.debug.print("Running command: {s}\n", .{args});
-  try process.spawn();
-  const ret_val = try process.wait();
-  std.debug.print("The command returned: {s}\n", .{tagName(ret_val)});
+    std.debug.print("Running command: {s}\n", .{args});
+    try process.spawn();
+    const ret_val = try process.wait();
+    std.debug.print("The command returned: {s}\n", .{tagName(ret_val)});
 }
